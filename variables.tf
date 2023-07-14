945613d5164442f3372881dafb18627c01b7a3e9
orochmarufromlinuxx <orochimarufromlinux@gmail.com>
2023-07-14 17:41:42 +0200

    terraform

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+}
+
+variable "team" {
+  default = "team1"
+}
+
+variable "protocol_type" {
+  default = "email"
+}
+
+variable "endpoint" {
+  default = "orochimarufromlinux@gmail.com"
+}
---
+++ version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.my_region
+  profile = var.profile
+}
+
